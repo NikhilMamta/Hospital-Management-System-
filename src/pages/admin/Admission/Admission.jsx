10ef16993a47b9b19fa1848ccf6a1345efebd695
bbalaji06 <bhupatibalajinaidu@gmail.com>
2026-03-19 13:11:52 +0530

    fixed add-patient input field

--- src/pages/admin/Admission/Admission.jsx
+++ src/pages/admin/Admission/Admission.jsx
@@ -277,9 +277,12 @@ const Admission = () => {
           <Search className="absolute w-5 h-5 text-gray-400 transform -translate-y-1/2 left-3 top-1/2" />
           <input
             type="text"
-            placeholder="Search by name, phone, admission no..."
             value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search by name, admission no.."
+            onChange={(e) => {
+              console.log("RAW:", e.target.value);
+              setSearchQuery(e.target.value);
+            }}
             className="w-full pl-10 pr-4 py-2.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 bg-white"
           />
         </div>
